a532c2121964f23329d12cf726b4f2c13504602e
Xiaotian Wu <yetist@gmail.com>
2023-06-14 21:54:52 +0800

    update

--- view.sh
+++ view.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-hugo --printI18nWarnings --disableFastRender --ignoreCache --forceSyncStatic server
+hugo --printI18nWarnings --disableFastRender --ignoreCache --forceSyncStatic server --bind 0.0.0.0
